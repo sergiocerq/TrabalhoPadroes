01954996c846e033421839c4d3e6f86f0baeca55
sergiocerq <sergiocerq11@gmail.com>
2024-09-01 21:49:52 -0300

    fix: corrigida interface do historico arquivo

--- src/br/ifba/inf011/aval2/model/memento/HistoricoArquivo.java
+++ src/br/ifba/inf011/aval2/model/memento/HistoricoArquivo.java
@@ -1,9 +1,13 @@
 package br.ifba.inf011.aval2.model.memento;
 
 import br.ifba.inf011.aval2.model.Credencial;
+import br.ifba.inf011.aval2.model.Entrada;
+import br.ifba.inf011.aval2.model.EntradaOperavel;
 import br.ifba.inf011.aval2.model.Operavel;
 import br.ifba.inf011.aval2.model.state.Arquivo;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Stack;
 
 /*
@@ -11,7 +15,7 @@ import java.util.Stack;
  *
  * - HistoricoArquivo: Caretaker que armazena os Mementos de um Arquivo
  */
-public class HistoricoArquivo implements Operavel {
+public class HistoricoArquivo implements EntradaOperavel {
 
   private Arquivo arquivo;
   private Stack<NarrowArquivo> mementos = new Stack<NarrowArquivo>();
@@ -44,5 +48,35 @@ public class HistoricoArquivo implements Operavel {
   @Override
   public void escrever(Credencial credencial, String escrever) throws IllegalAccessException {
     arquivo.escrever(credencial, escrever);
+  }
+
+  @Override
+  public String getNome() {
+    return arquivo.getNome();
+  }
+
+  @Override
+  public LocalDate getDataCriacao() {
+    return arquivo.getDataCriacao();
+  }
+
+  @Override
+  public Long getTamanho() {
+    return arquivo.getTamanho();
+  }
+
+  @Override
+  public List<Entrada> getFilhos() throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("Arquivo não pode ter filhos");
+  }
+
+  @Override
+  public void addFilho(Entrada entrada) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("Arquivo não pode ter filhos");
+  }
+
+  @Override
+  public void removeFilho(Entrada entrada) throws UnsupportedOperationException {
+    throw new UnsupportedOperationException("Arquivo não pode ter filhos");
   }
 }
